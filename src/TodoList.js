27bff2cec0ae454c23eb5c14cf236cf5bb950c4d
Oscar Espinoza <oscaranto.espinoza@gmail.com>
2023-11-03 00:34:47 -0300

    Fix: make all the components

---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+
+function TodoList({children}) {
+  return (
+    <ul>
+      {children}
+    </ul>
+  )
+}
+
+export {
+  TodoList
+}
---
+++ src/TodoSearch.js
@@ -0,0 +1,10 @@
+
+function TodoSearch() {
+  return (
+    <input type="text" placeholder="Cortar cebolla" />
+  )
+}
+
+export {
+  TodoSearch
+}
